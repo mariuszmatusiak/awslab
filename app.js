3f24f5c28bac7d4d7c0da819a50982adf084ed38
Mariusz <azamennace@gmail.com>
2015-10-19 11:24:06 +0200

    changes in lab2

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
+var mylab = require("./lab/mylab").lab;
+var AWS = require('aws-skd');
+
+AWS.config.loadFromPath('./config.json');
 
 var PORT = 8080;
 
@@ -7,10 +11,35 @@ var PORT = 8080;
 var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
-	{path: "/example_1", action: example_1}, 
+	{path: "/example_1", action: example_1},
+	{path: "/mylab", action: mylab}
 	];
 
 var service = require("./lib/service").http(urlMap);
 
 service(PORT);
 
+var params = {
+	DryRun: true || false,
+	Filters: [
+		{
+			Name: 'STRING_VALUE',
+			Values: [
+				'STRING_VALUE',
+				/*more intems */
+			]
+		},
+		/*more items*/
+	],
+	InstanceIds: [
+		'STRING_VALUE',
+		/*more intems*/
+	],
+	MaxResults: 0,
+	NextToken: 'STRING_VALUE'
+};
+
+describeInstances(params, function(err, data) {
+	if (err) console.log(err, err.stack);
+	else	console.log(data);
+});	
