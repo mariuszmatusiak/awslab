9b6ae69aad096e3472db9d3da930bc6d039b8a2f
Mariusz <azamennace@gmail.com>
2015-10-25 01:43:45 +0200

    fixes

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
-var mylab = require("./lab/mylab").lab;
-//var AWS = require('aws-skd');
-
-//AWS.config.loadFromPath('./config.json');
+var mylab = require("./lab/lab1_2").lab;
 
 var PORT = 8080;
-
 
 var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1},
-	{path: "/mylab", action: mylab}
+	{path: "/lab1_2", action: mylab}
 	];
 
 var service = require("./lib/service").http(urlMap);
--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -4,8 +4,11 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-	
-	
+	var params = {};
+	ec2.describeInstances(params, function(err, data){
+		if(err) callback(null, err+ ": " + err.stack);
+		else callback(null, data);
+	});
 }
 
 exports.lab = task
